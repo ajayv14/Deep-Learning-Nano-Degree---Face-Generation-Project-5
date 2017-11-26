815e45513c65b29907e96f296977a19658473d41
ajayvijayasarathy <ajay.v14@gmail.com>
2017-11-26 03:11:02 -0800

    optimized

--- helper (1).py
+++ helper (1).py
@@ -1,6 +1,6 @@
 <!DOCTYPE html><html lang="en" style="height:100%;width:100%"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1"><meta name="google-site-verification" content="ZkYgTr4uoN52h5RThSYRS8gyE44hWHqW3SANFoPeFuU"/><script type="text/javascript">
 //<![CDATA[
-window.__cfRocketOptions = {byc:0,p:0,petok:"19ea71ce0a46e633f75ddf8557e426cf503c1abd-1511693909-1800"};
+window.__cfRocketOptions = {byc:0,p:0,petok:"31a7cd356a069410bd0895873aedb8e3f01215bc-1511693938-1800"};
 //]]>
 </script>
 <script type="text/javascript" src="https://ajax.cloudflare.com/cdn-cgi/scripts/0e574bed/cloudflare-static/rocket.min.js"></script>
