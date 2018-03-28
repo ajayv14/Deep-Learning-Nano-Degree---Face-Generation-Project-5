669d5176994bfd6158cd71afc2a150a484c16c5f
ajayv14 <ajay.v14@gmail.com>
2018-03-27 17:26:59 -0700

    tested on gtx1080

--- helper (1).py
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html><html lang="en" style="height:100%;width:100%"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1"><meta name="google-site-verification" content="ZkYgTr4uoN52h5RThSYRS8gyE44hWHqW3SANFoPeFuU"/><script type="text/javascript">
-//<![CDATA[
-window.__cfRocketOptions = {byc:0,p:0,petok:"31a7cd356a069410bd0895873aedb8e3f01215bc-1511693938-1800"};
-//]]>
-</script>
-<script type="text/javascript" src="https://ajax.cloudflare.com/cdn-cgi/scripts/0e574bed/cloudflare-static/rocket.min.js"></script>
-<script data-rocketsrc="https://cdn.ravenjs.com/3.16.1/raven.min.js" crossorigin="anonymous" type="text/rocketscript"></script><script data-rocketsrc="https://js.stripe.com/v3/" type="text/rocketscript"></script><title>FloydHub - Deep Learning Platform - Cloud GPU</title><link rel="shortcut icon" href="/favicon.ico"><link href="/static/css/main.1b13ef46.css" rel="stylesheet"></head><body style="height:100%"><div id="app" style="height:100%;width:100%"></div><script type="text/rocketscript" data-rocketsrc="/static/js/main.fe132638.js"></script></body></html>
